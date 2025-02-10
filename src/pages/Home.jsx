e5e700dc2655e014562e1eb5351a2e95da2c7285
HimanshuJ011 <joshihimanshu245@gmail.com>
2025-02-10 17:40:20 +0530

    update theme

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import HeroSection from "../components/HeroSection";
 import NGOSections from "../components/NGOSections";
 
-
 const Home = () => {
   return (
     <>
       <HeroSection />
-      <div className="px-6 lg:px-8 ">
-        <NGOSections/>
-      </div>
+      <NGOSections />{" "}
     </>
   );
 };
